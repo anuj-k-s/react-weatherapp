5661647a7695208d5f4a8e1cce8cad1fd932a5da
anuj-k-s <anujkumarsoni1993@gmail.com>
2021-07-27 22:33:34 +0530

    usecallback hook to cache func avoding rerendering

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import Card from "../UI/Card/Card";
 import CityForm from "../CityForm/CityForm";
@@ -9,19 +9,19 @@ const Home = () => {
   const [cities, updateCities] = useState([]);
   console.log("home component loading");
 
-  useEffect(() => {
-    fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json")
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const dbCities = [];
-        for (const key in responseData) {
-          dbCities.push({ id: key, cityName: responseData[key].cityName, cityTemp: responseData[key].cityTemp });
-        }
-        updateCities(dbCities);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((responseData) => {
+  //       const dbCities = [];
+  //       for (const key in responseData) {
+  //         dbCities.push({ id: key, cityName: responseData[key].cityName, cityTemp: responseData[key].cityTemp });
+  //       }
+  //       updateCities(dbCities);
+  //     });
+  // }, []);
   /* 	Use Effect with empty [] will execute only once on load of component ,so will work like conmponentdidmount */
 
   useEffect(() => {
@@ -45,9 +45,17 @@ const Home = () => {
   const removeCityHandler = (id) => {
     updateCities((prevCities) => prevCities.filter((city) => city.id !== id));
   };
+
+  const filteredCitiesHandler = useCallback(
+    (filteredCities) => {
+      updateCities(filteredCities);
+    },
+    [updateCities]
+  );
+
   return (
     <React.Fragment>
-      <Search/>
+      <Search onLoadCities={filteredCitiesHandler} />
       <CityForm onAddCity={addCityHandler} />
       <section>
         <CityList cities={cities} onRemoveItem={removeCityHandler} />
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Search.module.css";
 import Card from "../UI/Card/Card";
+
 const Search = (props) => {
+  
+  const [enteredFilter, setEnteredFilter] = useState("");
+  const {onLoadCities} = props;
+  
+  useEffect(() => {
+    const query = enteredFilter.length === 0 ? '' : `?orderBy="cityName"&equalTo="${enteredFilter}"`;
+    fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json" + query)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const dbCities = [];
+        for (const key in responseData) {
+          dbCities.push({ id: key, cityName: responseData[key].cityName, cityTemp: responseData[key].cityTemp });
+        }
+        onLoadCities(dbCities);
+      });
+  }, [enteredFilter,onLoadCities]);
+  
   return (
     <section className={styles.search}>
       <Card>
         <div className={styles.search_bar}>
           <label htmlFor="cty">Search City</label>
-          <input type="text" id="cty" />
+          <input
+            type="text"
+            id="cty"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
