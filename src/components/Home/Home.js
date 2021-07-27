0a982e51d20bb8d5b02b7882df29f0f2137583d5
anuj-k-s <anujkumarsoni1993@gmail.com>
2021-07-27 12:01:47 +0530

    posting data to firebase cloud storage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Home.module.css";
-import  Card  from "../UI/Card/Card";
+import Card from "../UI/Card/Card";
 import CityForm from "../CityForm/CityForm";
 import CityList from "../CityList/CityList";
 const Home = () => {
-  
-  const [cities,updateCities] =  useState([]);
+  const [cities, updateCities] = useState([]);
  
-  const addCityHandler = city => {
-   updateCities(prevCities => [
-    ...prevCities,{id:Math.random().toString(),...city}
-   ]);
+  const addCityHandler = (city) => {
+    fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json", {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify(city),
+      headers: { "Content-Type": "application/json" },
+    }).then(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+    updateCities((prevCities) => [...prevCities, { id: Math.random().toString(), ...city }]);
   };
 
-
   const removeCityHandler = (id) => {
-    updateCities( prevCities =>
-       prevCities.filter(city => city.id !== id))
-  }
+    updateCities((prevCities) => prevCities.filter((city) => city.id !== id));
+  };
   return (
     <React.Fragment>
-      <CityForm onAddCity={addCityHandler}/>
+      <CityForm onAddCity={addCityHandler} />
       <section>
-        <CityList cities = {cities} onRemoveItem={removeCityHandler}/>
+        <CityList cities={cities} onRemoveItem={removeCityHandler} />
       </section>
-      
     </React.Fragment>
   );
 };
