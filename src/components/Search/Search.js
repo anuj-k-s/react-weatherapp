bf21fa0d8ed266e75e2dd52344fcec22196824f2
anuj-k-s <anujkumarsoni1993@gmail.com>
2021-07-27 22:46:25 +0530

    settimeout and cleanup timeout for multiple ajax

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,33 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Search.module.css";
 import Card from "../UI/Card/Card";
 
 const Search = (props) => {
-  
   const [enteredFilter, setEnteredFilter] = useState("");
-  const {onLoadCities} = props;
-  
+  const { onLoadCities } = props;
+  const inputRef = useRef();
+
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="cityName"&equalTo="${enteredFilter}"`;
-    fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json" + query)
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const dbCities = [];
-        for (const key in responseData) {
-          dbCities.push({ id: key, cityName: responseData[key].cityName, cityTemp: responseData[key].cityTemp });
-        }
-        onLoadCities(dbCities);
-      });
-  }, [enteredFilter,onLoadCities]);
-  
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 ? "" : `?orderBy="cityName"&equalTo="${enteredFilter}"`;
+        fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json" + query)
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const dbCities = [];
+            for (const key in responseData) {
+              dbCities.push({ id: key, cityName: responseData[key].cityName, cityTemp: responseData[key].cityTemp });
+            }
+            onLoadCities(dbCities);
+          });
+      }
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadCities]);
+
   return (
     <section className={styles.search}>
       <Card>
         <div className={styles.search_bar}>
           <label htmlFor="cty">Search City</label>
           <input
+            ref={inputRef}
             type="text"
             id="cty"
             value={enteredFilter}
