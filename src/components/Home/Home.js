71e8ad10972f96cd11482184f4bfd7b2017fbe24
anuj-k-s <anujkumarsoni1993@gmail.com>
2021-07-28 23:00:46 +0530

    useReducer hook to merge mltple dependend states

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,44 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 import CityForm from "../CityForm/CityForm";
 import CityList from "../CityList/CityList";
 import Search from "../Search/Search";
 import ErrorModal from "../UI/ErrorModal/ErrorModal";
 
-const Home = () => {
+const cityReducer = (currentCities, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.cities;
+    case "ADD":
+      return [...currentCities, action.addedCity];
+    case "DELETE":
+      return currentCities.filter((city) => city.id !== action.id);
+    default:
+      throw new Error("Not possible");
+  }
+};
 
-  const [cities, updateCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  console.log(isLoading);
+const httpReducer = (currentHttpState, action) => {
+  switch (action.type) {
+    case "SEND":
+      return { loading: true, error: null };
+    case "RESPONSE":
+      return { ...currentHttpState, loading: false };
+    case "ERROR":
+      return { ...currentHttpState, loading: false, error: action.errorData };
+    case "CLEAR":
+      return { ...currentHttpState, error: null };
+    default:
+      throw new Error("Not possible");
+  }
+};
+const Home = () => {
+  //const [cities, updateCities] = useState([]);
+  const [cities, dispatch] = useReducer(cityReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null });
+  /* when we have multiple states / complex state  which can update simultaneously and depended on each other,better to use UseReducer */
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+  console.log(httpState);
   console.log("home component loading");
 
   // useEffect(() => {
@@ -33,7 +62,8 @@ const Home = () => {
   /* 	Use Effect with empty array will execute on every render cycle and works like componentdidupdate */
 
   const addCityHandler = (city) => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    dispatchHttp({ type: "SEND" });
     fetch("https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/.json", {
       method: "POST",
       body: JSON.stringify(city),
@@ -41,43 +71,46 @@ const Home = () => {
     })
       .then((response) => response.json())
       .then((responseData) => {
-        setIsLoading(false);
-        updateCities((prevCities) => [...prevCities, { id: responseData.name, ...city }]);
+        //setIsLoading(false);
+        dispatchHttp({ type: "RESPONSE" });
+        dispatch({ type: "ADD", addedCity: { id: responseData.name, ...city } });
+        //updateCities((prevCities) => [...prevCities, { id: responseData.name, ...city }]);
       });
   };
 
   const removeCityHandler = (id) => {
-    debugger;
-    setIsLoading(true);
-    fetch(`https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/${id}.jso`, {
+    // setIsLoading(true);
+    dispatchHttp({ type: "SEND" });
+    fetch(`https://react-hooks-45fc8-default-rtdb.asia-southeast1.firebasedatabase.app/${id}.json`, {
       method: "DELETE",
     })
       .then((response) => {
-        setIsLoading(false);
-        updateCities((prevCities) => prevCities.filter((city) => city.id !== id));
+        // setIsLoading(false);
+        //updateCities((prevCities) => prevCities.filter((city) => city.id !== id));
+        dispatchHttp({ type: "RESPONSE" });
+        dispatch({ type: "DELETE", id });
       })
       .catch((error) => {
-        setIsLoading(false);
-        setError(error.message);
+        dispatchHttp({ type: "ERROR", errorData: error.message });
+        // setIsLoading(false);
+        // setError(error.message);
       });
   };
 
-  const filteredCitiesHandler = useCallback(
-    (filteredCities) => {
-      updateCities(filteredCities);
-    },
-    [updateCities]
-  );
- 
+  const filteredCitiesHandler = useCallback((filteredCities) => {
+    dispatch({ type: "SET", cities: filteredCities });
+    //updateCities(filteredCities);
+  }, []);
+
   const clearError = () => {
-    setError(null);
-    
-  }
+    // setError(null);
+    dispatchHttp({type:'CLEAR'});
+  };
   return (
     <React.Fragment>
-      {error && <ErrorModal onClose={clearError} >{error}</ErrorModal>}
-      <Search onLoadCities={filteredCitiesHandler} loading={isLoading} />
-      <CityForm onAddCity={addCityHandler} loading={isLoading} />
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <Search onLoadCities={filteredCitiesHandler} />
+      <CityForm onAddCity={addCityHandler} loading={httpState.loading} />
       <section>
         <CityList cities={cities} onRemoveItem={removeCityHandler} />
       </section>
